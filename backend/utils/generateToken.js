e43beb5149a7b65ea5d256a51ec613341bcc7f25
Aayush <aayush.dl8901@gmail.com>
2025-02-04 16:05:20 +0530

    update same site in cookie option

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -12,7 +12,7 @@ exports.sendToken = (user, statusCode, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "none",
+    sameSite: "strict",
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
   };
 
