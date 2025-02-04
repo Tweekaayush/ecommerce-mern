c63ba828cc533fd8828f12eacb64d0d269d97ace
Aayush <aayush.dl8901@gmail.com>
2025-02-04 16:22:13 +0530

    update cookie option

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -10,7 +10,7 @@ exports.sendToken = (user, statusCode, res) => {
   );
 
   const options = {
-    httpOnly: true,
+    // httpOnly: true,
     secure: true,
     sameSite: "strict",
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
