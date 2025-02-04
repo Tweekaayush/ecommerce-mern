29dda2ff4c731fba00207062c729f55f36511824
Aayush <aayush.dl8901@gmail.com>
2025-02-04 15:22:17 +0530

    testing backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 const express = require('express')
-const dotenv = require('dotenv')
-dotenv.config()
-const connectDb = require('./config/db')
-const product = require('./routes/productRoutes')
-const user = require('./routes/userRoutes')
-const order = require('./routes/orderRoutes')
-const payment = require('./routes/paymentRoutes')
-const {notFound, errorHandler} = require('./middleware/error')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const PORT = process.env.PORT || 5000
-// const cloudinary = require('cloudinary')
-const fileUpload = require("express-fileupload")
+// dotenv.config()
+// const dotenv = require('dotenv')
+// const connectDb = require('./config/db')
+// const product = require('./routes/productRoutes')
+// const user = require('./routes/userRoutes')
+// const order = require('./routes/orderRoutes')
+// const payment = require('./routes/paymentRoutes')
+// const {notFound, errorHandler} = require('./middleware/error')
+// const cors = require('cors')
+// const cookieParser = require('cookie-parser')
+// // const cloudinary = require('cloudinary')
+// const fileUpload = require("express-fileupload")
+// const PORT = process.env.PORT || 5000
+const PORT = 5000
 const app = express()
 
 
